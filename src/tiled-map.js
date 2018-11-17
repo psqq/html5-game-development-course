95cfeed4c67b267d11cceffb68fa36d635c0d9c5
psqq <pestov-sa@ya.ru>
2018-11-17 19:57:20 +0300

    Loading Tileset Data

--- src/tiled-map.js
+++ src/tiled-map.js
@@ -1,3 +1,5 @@
+import path from 'path-browserify';
+import { loadImage } from './images';
 
 export default class TiledMap {
     constructor(filename) {
@@ -20,5 +22,10 @@ export default class TiledMap {
             x: this.numXTiles * this.tileSize.x,
             y: this.numYTiles * this.tileSize.y
         };
+        for(var tileset of json.tilesets) {
+            var curDir = path.dirname(this.filename);
+            var fn = path.join(curDir, tileset.image);
+            var img = await loadImage(fn);
+        }
     }
 }
