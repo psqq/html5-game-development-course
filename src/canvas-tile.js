b584d56f42460f7b300cf775a754b4e5c18dfcb7
psqq <pestov-sa@ya.ru>
2018-11-18 12:50:24 +0300

    Offscreen canvas

---
+++ src/canvas-tile.js
@@ -0,0 +1,13 @@
+
+export default class CanvasTile {
+    constructor(width, height) {
+        this.x = -1;
+        this.y = -1;
+        this.w = width;
+        this.h = height;
+        this.can = document.createElement('canvas');
+        this.ctx = this.can.getContext('2d');
+        this.can.width = width;
+        this.can.height = height;
+    }
+}
